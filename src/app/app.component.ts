1875778ba7c7efd97cd24c0265dd8823200238fa
pecsukj_training <Administrator@EC2AMAZ-9K7FIUU>
2024-10-10 09:08:36 +0000

    http call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   template: `<div>Hello {{value}}</div>`,
 })
 export class AppComponent {
+  constructor(private http: HttpClient) { }
   value = 'Azure training ';
+  https://angulardemowelcome.azurewebsites.net/
+   this.http.get("https://angulardemowelcome.azurewebsites.net/api/SendEmail?").subscribe(data=>{
+    console.log(JSON.stringify(data));
+   });
 }
