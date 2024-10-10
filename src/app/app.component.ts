40baebfde43b08d1858957c060d02442b109d913
pecsukj_training <Administrator@EC2AMAZ-9K7FIUU>
2024-10-10 09:27:37 +0000

    fixing http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   template: `<div>Hello {{value}}</div>`,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(private http: HttpClient) { }
+  ngOnInit() {
+    // ...
+   // https://angulardemowelcome.azurewebsites.net/
+    this.http.get("https://angulardemowelcome.azurewebsites.net/api/SendEmail?").subscribe(data=>{
+     console.log(JSON.stringify(data));
+    });
+  }
   value = 'Azure training ';
-  https://angulardemowelcome.azurewebsites.net/
-   this.http.get("https://angulardemowelcome.azurewebsites.net/api/SendEmail?").subscribe(data=>{
-    console.log(JSON.stringify(data));
-   });
+ 
 }
